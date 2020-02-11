f456a0bd61c8f2461fc2ce7780bd0b6396df3086
Heart <dilsherb13@gmail.com>
2020-02-11 15:50:27 -0800

    Hmm

--- Twitter/homeTableViewController.swift
+++ Twitter/homeTableViewController.swift
@@ -30,6 +30,11 @@ class homeTableViewController: UITableViewController {
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
 
     
     @objc func loadTweets(){
